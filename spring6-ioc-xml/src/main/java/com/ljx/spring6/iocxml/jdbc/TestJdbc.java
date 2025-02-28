35d88395b21283820de480a4c2ff9a681de9c0d8
lijiaxiang_hela <1259465943@qq.com>
2025-02-28 16:37:25 +0800

    spring中bean对象Ioc容器引入外部属性文件注入

---
+++ spring6-ioc-xml/src/main/java/com/ljx/spring6/iocxml/jdbc/TestJdbc.java
@@ -0,0 +1,27 @@
+package com.ljx.spring6.iocxml.jdbc;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestJdbc {
+
+    /*原生方法，不用bean注入*/
+    @Test
+    public void demo1(){
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setUrl("jdbc:mysql://localhost:3306/ssm?serverTimezone=UTC");
+        dataSource.setUsername("root");
+        dataSource.setPassword("123456");
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+    }
+
+    @Test
+    public void demo2(){
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("bean-jdbc.xml");
+        DruidDataSource dataSource = context.getBean(DruidDataSource.class);
+        System.out.println(dataSource.getUrl());
+    }
+}
