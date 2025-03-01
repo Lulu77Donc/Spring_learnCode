a180335ff8a4bbcedff84d381591438287161226
lijiaxiang_hela <1259465943@qq.com>
2025-03-01 16:06:49 +0800

    @Autowired注解和@Qualifier注解联合注入

--- spring6-ioc-annotation/src/main/java/com/ljx/spring6/autowired/Service/UserServiceImpl.java
+++ spring6-ioc-annotation/src/main/java/com/ljx/spring6/autowired/Service/UserServiceImpl.java
@@ -2,13 +2,16 @@ package com.ljx.spring6.autowired.Service;
 
 import com.ljx.spring6.autowired.Dao.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService{
 
     //注入dao
+    //两个注解，根据名称注入
     @Autowired
+    @Qualifier(value = "userRedisDaoImpl")
     private UserDao userDao;
 
     @Override
